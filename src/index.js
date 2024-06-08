590837a3350e1a7b640a18d8aee65363cb49e1d6
Lamia Fatiha <f6lamia@gmail.com>
2024-06-08 21:33:13 +0600

    first JSX challenge done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  [
+    <h1>This is my First JSX challenge</h1>,
+    <p>Here is the five netflix series list:</p>,
+    <ol>
+      <li>Nowhere</li>
+      <li>Dark</li>
+      <li>Stranger Things</li>
+      <li>Mr Robot</li>
+      <li>Hello</li>
+    </ol>
+  ],
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
